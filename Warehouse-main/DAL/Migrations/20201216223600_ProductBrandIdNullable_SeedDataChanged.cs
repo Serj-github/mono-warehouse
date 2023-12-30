bd983006498f38843944d1d066b46523533085a4
Serhii <gopanchuk-ukrnet@ukr.net>
2023-12-30 14:25:53 +0200

    init commit

---
+++ Warehouse-main/DAL/Migrations/20201216223600_ProductBrandIdNullable_SeedDataChanged.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class ProductBrandIdNullable_SeedDataChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BrandId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Suppliers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CompanyName",
+                value: "Mason company");
+
+            migrationBuilder.UpdateData(
+                table: "Suppliers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CompanyName",
+                value: "Hudson warehouse");
+
+            migrationBuilder.UpdateData(
+                table: "Suppliers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CompanyName",
+                value: "Woods storage");
+
+            migrationBuilder.UpdateData(
+                table: "Suppliers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CompanyName",
+                value: "Bowman delivery");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BrandId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Suppliers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CompanyName",
+                value: "OOO \"Mason\"");
+
+            migrationBuilder.UpdateData(
+                table: "Suppliers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CompanyName",
+                value: "OOO \"Hudson\"");
+
+            migrationBuilder.UpdateData(
+                table: "Suppliers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CompanyName",
+                value: "OOO \"Woods\"");
+
+            migrationBuilder.UpdateData(
+                table: "Suppliers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CompanyName",
+                value: "FLP \"Bowman\"");
+        }
+    }
+}
